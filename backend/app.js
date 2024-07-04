ab6ed3bc17baff48c383a54ecd50a29889d295e4
Augustofrx <augustoiphar99@hotmail.com>
2024-07-04 12:12:36 -0300

    initializing boiler plate

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
