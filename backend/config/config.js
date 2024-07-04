eddb490e07ef4b078525fe1f122d16d8cbf7d27b
Augustofrx <augustoiphar99@hotmail.com>
2024-07-04 12:24:05 -0300

    add sequelize and postgres dependencies

---
+++ backend/config/config.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_TEST,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_PRODUCTION,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  },
+};
