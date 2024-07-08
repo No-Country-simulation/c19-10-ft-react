47d90f4c9c3355c4975e93389cf5f011b7f82a8d
Augustofrx <augustoiphar99@hotmail.com>
2024-07-08 19:18:06 -0300

    add: models auto import from db connection

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,9 +1,10 @@
 const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
-const config = require(path.resolve(__dirname, "../config/config.js"))[
-  process.env.NODE_ENV || "development"
-];
+const fs = require("fs");
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "development";
+const config = require(path.resolve(__dirname, "../config/config.js"))[env];
 const db = {};
 
 const sequelize = new Sequelize(
@@ -13,6 +14,7 @@ const sequelize = new Sequelize(
   config
 );
 
+// Leer todos los archivos de modelos en la carpeta 'models' y agregarlos al objeto db
 fs.readdirSync(__dirname)
   .filter((file) => {
     return (
