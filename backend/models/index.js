413db8cf2ca1bbaa25d37a66660f6a2d3ddb7bd6
Augustofrx <augustoiphar99@hotmail.com>
2024-07-08 19:15:24 -0300

    add: models auto import from db connection

--- backend/models/index.js
+++ backend/models/index.js
@@ -13,6 +13,26 @@ const sequelize = new Sequelize(
   config
 );
 
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
