38083959d9d95ed11b1f540da0f7e91bf1627a64
Augustofrx <augustoiphar99@hotmail.com>
2024-07-09 10:46:10 -0300

    fix: initial folders structure config

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,8 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold">Hello ! this is Celebria App 😁</h1>
+    </div>
+  );
+}
