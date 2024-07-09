f5f3e95c536d6eb341f82af03982c2f9bade6b99
Gisella Ortiz de la Tabla <gisellanalia@gmail.com>
2024-07-09 21:13:48 +0200

    Create Navbar component

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,8 +1,14 @@
 // pages/index.js
+
+import NavBar from "../components/NavBar";
+
 export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold">Hello ! this is Celebria App 😁</h1>
-    </div>
-  );
+	return (
+		<div>
+			<NavBar/>
+			<div className="min-h-screen flex items-center justify-center bg-gray-100">
+				<h1 className="text-4xl font-bold">Hello! This is Celebria App 😁</h1>
+			</div>
+		</div>
+	);
 }
