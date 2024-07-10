3e0dd0a019b6d67dd224d22da60003e984d4b220
ManzanoFernanda <manzanofernandadaniela@gmail.com>
2024-07-10 13:18:01 -0300

    Cambio de fondo

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -3,8 +3,8 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
-import Logo from '../../public/Celebria-logo.webp';
-import BackgroundImage from '../../public/Fondo.jpg';
+import Logo from '../../public/CelebriaWhite.png';
+import BackgroundImage from '../../public/party.jpg';
 
 const LoginSchema = Yup.object().shape({
   email: Yup.string().email('Correo electrónico inválido').required('Campo requerido'),
@@ -35,7 +35,7 @@ const LoginPage = () => {
         {/*esquina superior derecha */}
         <div className="flex items-center">
           <p className="text-23 font-bold text-white mr-2">¿No te has registrado?</p>
-          <button className="bg-accent text-white py-2 px-4 rounded-md hover:bg-primary focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100 font-bold text-base">
+          <button className="bg-accent text-white py-2 px-4 rounded-md hover:bg-primary focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:ring-offset-gray-100 font-bold text-base">
             Registrarse
           </button>
         </div>
@@ -57,16 +57,16 @@ const LoginPage = () => {
             <Form>
               <div className="mb-6">
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">Correo Electrónico</label>
-                <Field type="email" id="email" name="email" className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-white border-grey border-2 py-2 px-3" />
+                <Field type="email" id="email" name="email" className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-primary focus:border-primary sm:text-sm bg-white border-grey border-2 py-2 px-3" />
                 <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
               </div>
               <div className="mb-6">
                 <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">Contraseña</label>
                 <div className="relative">
-                  <Field type={showPassword ? "text" : "password"} id="password" name="password" className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-white border-grey border-2 py-2 px-3" />
+                  <Field type={showPassword ? "text" : "password"} id="password" name="password" className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-primary focus:border-indigo-500 sm:text-sm bg-white border-grey border-2 py-2 px-3" />
                   <button
                     type="button"
-                    className="absolute inset-y-0 right-0 px-4 py-2"
+                    className="absolute inset-y-0 right-0 px-4 py-2 bold text-sm primary focus:outline-none hover:text-primary"
                     onClick={togglePasswordVisibility}
                   >
                     {showPassword ? "Ocultar" : "Mostrar"}
@@ -74,7 +74,7 @@ const LoginPage = () => {
                 </div>
                 <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
               </div>
-              <button type="submit" className="w-full bg-primary text-white py-2 px-4 rounded-md hover:bg-accent focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100" disabled={isSubmitting}>
+              <button type="submit" className="w-full bg-primary text-white py-2 px-4 rounded-md hover:bg-accent focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:ring-offset-gray-100" disabled={isSubmitting}>
                 {isSubmitting ? 'Cargando...' : 'Iniciar Sesión'}
               </button>
             </Form>
