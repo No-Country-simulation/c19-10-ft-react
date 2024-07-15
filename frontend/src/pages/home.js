3b8613d2ecb82ef6512fc56562850ddf9ee7fcc1
ManzanoFernanda <manzanofernandadaniela@gmail.com>
2024-07-15 10:50:35 -0300

    modal-mejorado

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -43,6 +43,9 @@ const Home = () => {
     router.push("/");
   };
 
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
   return (
     <div className="w-full h-full flex">
       <aside className="flex flex-col justify-between gap-4 bg-primary py-4 px-2 min-h-screen">
@@ -52,33 +55,13 @@ const Home = () => {
         <div className="divider divider-neutral opacity-50"></div>
 
         <section className="min-h-[660px] flex flex-col gap-8">
-          <MenuOption
-            logo={ProfileLogo}
-            alt={"profile-logo"}
-            label={"Perfil"}
-          />
-          <MenuOption
-            logo={NotificationsLogo}
-            alt={"notifications-logo"}
-            label={"Avisos"}
-          />
-          <MenuOption
-            logo={CalendarLogo}
-            alt={"calendar-logo"}
-            label={"Calendario"}
-          />
-          <MenuOption
-            logo={InvitationLogo}
-            alt={"invitation-logo"}
-            label={"Invitaciones"}
-          />
+          <MenuOption logo={ProfileLogo} alt={"profile-logo"} label={"Perfil"} />
+          <MenuOption logo={NotificationsLogo} alt={"notifications-logo"} label={"Avisos"} />
+          <MenuOption logo={CalendarLogo} alt={"calendar-logo"} label={"Calendario"} />
+          <MenuOption logo={InvitationLogo} alt={"invitation-logo"} label={"Invitaciones"} />
         </section>
         <section className="h-full w-full">
-          <MenuOption
-            logo={SettingsLogo}
-            alt={"settings-logo"}
-            label={"Configuración"}
-          />
+          <MenuOption logo={SettingsLogo} alt={"settings-logo"} label={"Configuración"} />
         </section>
       </aside>
       <section className="w-full h-full py-12 px-56 flex flex-col justify-center items-center gap-4">
@@ -101,11 +84,7 @@ const Home = () => {
             title={"Registra tu evento"}
             onClick={openModal} // Asegúrate de pasar la función onClick aquí
           />
-          <HomeCard
-            image={YourEventsBackground}
-            size={1}
-            title={"Tus eventos"}
-          />
+          <HomeCard image={YourEventsBackground} size={1} title={"Tus eventos"} />
         </section>
         <section className="w-full h-full flex flex-col justify-center items-center">
           <div className="w-full">
@@ -121,68 +100,81 @@ const Home = () => {
         </section>
       </section>
 
-      <div className={`modal ${isModalOpen ? "modal-open" : ""}`}>
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Registra tu evento</h3>
-          <form className="flex flex-col gap-4">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Tipo de Evento</span>
-              </label>
-              <select
-                className="select select-bordered focus:border-primary"
-                value={eventType}
-                onChange={(e) => setEventType(e.target.value)}
-              >
-                <option value="">Seleccione un tipo de evento</option>
-                <option value="casamiento">Casamiento</option>
-                <option value="cumpleaños infantil">Cumpleaños Infantil</option>
-                <option value="cumpleaños de 15">Cumpleaños de 15</option>
-                <option value="cumpleaños adulto">Cumpleaños Adulto</option>
-                <option value="baby shower">Baby Shower</option>
-                <option value="despedida de soltero/a">Despedida de Soltero/a</option>
-                <option value="evento empresarial">Evento Empresarial</option>
-              </select>
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Nombre del Evento</span>
-              </label>
-              <input
-                type="text"
-                className="input input-bordered focus:border-primary"
-                value={eventName}
-                onChange={(e) => setEventName(e.target.value)}
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Descripción</span>
-              </label>
-              <textarea
-                className="textarea textarea-bordered focus:border-primary"
-                value={eventDescription}
-                onChange={(e) => setEventDescription(e.target.value)}
-              ></textarea>
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Fecha</span>
-              </label>
-              <DatePicker
-                selected={eventDate}
-                onChange={(date) => setEventDate(date)}
-                className="input input-bordered focus:border-primary"
-                dateFormat="dd/MM/yyyy"
-              />
-            </div>
-            <div className="modal-action">
-              <button type="button" className="btn btn-secondary" onClick={closeModal}>Cerrar</button>
-              <button type="submit" className="btn btn-primary">Guardar</button>
-            </div>
-          </form>
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+          <div className="bg-white rounded-lg p-6 w-1/2">
+            <h2 className="text-2xl mb-4">Registra tu evento</h2>
+            <form>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Tipo de evento
+                </label>
+                <select
+                  className="mt-1 block w-full h-12 border border-primary rounded-md shadow-sm focus:border-primary focus:ring-2 focus:ring-primary focus:outline-none"
+                  value={eventType}
+                  onChange={(e) => setEventType(e.target.value)}
+                >
+                  <option>Seleccione un tipo de evento</option>
+                  <option>Casamiento</option>
+                  <option>Cumpleaños Infantil</option>
+                  <option>Cumpleaños de 15</option>
+                  <option>Cumpleaños Adulto</option>
+                  <option>Baby Shower</option>
+                  <option>Despedida de soltero/a</option>
+                  <option>Evento empresarial</option>
+                </select>
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Nombre del evento
+                </label>
+                <input
+                  type="text"
+                  className="mt-1 block w-full h-12 border border-primary rounded-md shadow-sm focus:border-primary focus:ring-2 focus:ring-primary focus:outline-none"
+                  value={eventName}
+                  onChange={(e) => setEventName(e.target.value)}
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Descripción
+                </label>
+                <textarea
+                  className="mt-1 block w-full h-24 border border-primary rounded-md shadow-sm focus:border-primary focus:ring-2 focus:ring-primary focus:outline-none"
+                  value={eventDescription}
+                  onChange={(e) => setEventDescription(e.target.value)}
+                />
+              </div>
+              <div className="mb-4 flex flex-col items-center">
+                <label className="block text-sm font-medium text-gray-700">
+                  Fecha
+                </label>
+                <DatePicker
+                  className="mt-1 block w-full h-12 border border-primary rounded-md shadow-sm text-center focus:border-primary focus:ring-2 focus:ring-primary focus:outline-none"
+                  selected={eventDate}
+                  onChange={(date) => setEventDate(date)}
+                />
+              </div>
+              <div className="flex justify-end">
+                <button
+                  type="button"
+                  onClick={closeModal}
+                  className="bg-red-500 text-white px-4 py-2 rounded-md"
+                >
+                  Cancelar
+                </button>
+                <button
+                  type="submit"
+                  className="bg-primary text-white px-4 py-2 ml-4 rounded-md"
+                >
+                  Guardar
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
