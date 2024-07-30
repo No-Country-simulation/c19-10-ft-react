66cf640727b27127cdfa66bc5f8e434781e12bc8
Augustofrx <augustoiphar99@hotmail.com>
2024-07-30 10:22:54 -0300

    refactor: out of use files were deleted and models were modified

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,26 +2,21 @@ const express = require("express");
 
 const usersRouter = require("./users.router");
 const eventRouter = require("./event.router");
-const messageRouter = require("./message.router");
+const commentsRouter = require("./comment.router");
 const invitationRouter = require("./invitation.router");
 const postRouter = require("./post.router");
-const postImagesRouter = require("./postImages.router");
-const imageRouter = require("./image.router")
-const donationRouter = require("./donation.router")
 
+const donationRouter = require("./donation.router");
 
 function routerApi(app) {
   const router = express.Router();
   app.use("/api/v1", router);
   router.use("/users", usersRouter);
   router.use("/event", eventRouter);
-  router.use("/messages", messageRouter);
+  router.use("/comment", commentsRouter);
   router.use("/invitation", invitationRouter);
   router.use("/post", postRouter);
-  router.use("/image", imageRouter);
-  router.use("/post_images", postImagesRouter);
   router.use("/donation", donationRouter);
-  router.use("/images", postImagesRouter);
 }
 
 module.exports = routerApi;
